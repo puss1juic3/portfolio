5767e33b97e0a516df17a27e7fe50b1455eeaa6b
puss1juic3 <puss1juic3@gmail.com>
2020-04-23 13:28:44 +0300

    Slick dots customize

--- src/assets/js/components/slick.js
+++ src/assets/js/components/slick.js
@@ -10,6 +10,21 @@ contentSlider.slick({
     verticalScrolling: true,
     swipe:true,
     swipeToSlide:true,
+    customPaging: function(slick,index) {
+        switch (index) {
+            case 0:
+                return '<span class="numbers">' + '0' + (index + 1) + '</span>' + '<span class="text">Home</span>';
+            case 1:
+                return '<span class="numbers">' + '0' + (index + 1) + '</span>' + '<span class="text">Works</span>';
+            case 2:
+                return '<span class="numbers">' + '0' + (index + 1) + '</span>' + '<span class="text">About</span>';
+            case 3:
+                return '<span class="numbers">' + '0' + (index + 1) + '</span>' + '<span class="text">Contacts</span>';
+            case 4:
+                return '<span class="numbers">' + '0' + (index + 1) + '</span>' + '<span class="text">Hire us</span>';
+        }
+
+    }
 });
 
 contentSlider.on('wheel', (function(e) {
